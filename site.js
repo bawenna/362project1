12ca410303c94d6ca8e795052c3e59d5f5e25d77
bawenna <wzhang87@hawk.iit.edu>
2018-02-04 19:29:06 -0600

    FIxed message

--- site.js
+++ site.js
@@ -4,24 +4,24 @@ $('#signup').on('submit',
     var emailVal = $('#email').val();
     var phonePattern = /\d{3}[\-]\d{3}[\-]\d{4}/;
     var phoneVal = $('#phone').val();
+    var msg = '<p id="msg"> The form was submitted! </p>';
     e.preventDefault();
-    if(!(phoneVal.match(phonePattern)))
-    {
-      console.log("Trying");
-      alert('Please match the phone format of "xxx-xxx-xxxx"');
-      return false;
-    }
     if(!(emailVal.match(emailPattern)))
     {
       console.log("Trying to validate email");
       alert('Please match the email format of "you@example.com"');
       return false;
     }
-
+    if(!(phoneVal.match(phonePattern)))
+    {
+      console.log("Trying");
+      alert('Please match the phone format of "xxx-xxx-xxxx"');
+      return false;
+    }
     else
     {
     document.getElementById('submit').style.visibility = 'hidden';
-    $('#signup').append('The form was submitted!');
+    $('#signup').append(msg);
     }
   }
 );
