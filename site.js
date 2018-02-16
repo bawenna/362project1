843d331403ced68dd31c6d81ca92da0a2e885693
bawenna <wzhang87@hawk.iit.edu>
2018-02-16 13:08:29 -0600

    Applying a new design

--- site.js
+++ site.js
@@ -7,6 +7,10 @@ $('#signup').on('submit',
     var interest = {
       val: $('#interest').val()
     }
+    var condition = {
+      email: 0,
+      interest: 0
+    }
     var msg = '<p id="msg"> The form was submitted! </p>';
     var email_fail = '<p class="false">Please match the email format of "you@example.com"</p>';
     var interest_fail = '<p class="false">Please select one from the drop down menu</p>'
@@ -18,13 +22,19 @@ $('#signup').on('submit',
       $('#emailsec').append(email_fail);
       return false;
     }
+    else {
+      condition.email = 1;
+    }
     if((interest.val) === "0")
     {
       $('.false').remove();
       $('#deals').append(interest_fail);
       return false;
     }
-    else
+    else {
+      condition.interest = 1;
+    }
+    if(condition.email === 1 && condition.interest === 1)
     {
     $('.false').remove();
     $('#submit').hide();
